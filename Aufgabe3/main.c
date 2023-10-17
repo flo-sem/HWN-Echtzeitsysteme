e206eb943792c50cd529bf1a2444eab0fb5eb803
robinpfister <119607482+robinpfister@users.noreply.github.com>
2023-10-17 10:22:57 +0200

    whitespace correction

--- Aufgabe3/main.c
+++ Aufgabe3/main.c
@@ -4,13 +4,13 @@
 
 // Projekt für Aufgabe 3
 // 		Aufgaben:
-//		-  User Button über Interrupt erkennen 						DONE
-//		-  GPIOD & GPIOA Konfigurieren 										DONE
-//		-  NVIC Konfigurieren 														DONE
-//		-  ISR anlegen 																		DONE
-//		-  Interrupt sperren und freigeben 								DONE
+//		-  User Button über Interrupt erkennen 				DONE
+//		-  GPIOD & GPIOA Konfigurieren 						DONE
+//		-  NVIC Konfigurieren 								DONE
+//		-  ISR anlegen 										DONE
+//		-  Interrupt sperren und freigeben 					DONE
 //		-  Button-Event in statischer Variable speichern 	DONE
-//		-  LED an und aus toggeln 												DONE
+//		-  LED an und aus toggeln 							DONE
 
 volatile static int state = 0;	//0=main , 1=interrupt
 
@@ -21,24 +21,24 @@ void EXTI0_IRQHandler() {
 	if(EXTI->PR & EXTI_PR_PR0) {		// EXTI_PR wird 1 wenn der Interrupt ausgelöst wird
 		++state;
 		state=state%2;
-		EXTI->PR |= EXTI_PR_PR0;			// EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
+		EXTI->PR |= EXTI_PR_PR0;		// EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
 	}
 }
 int main() {
-	RCC->AHB1ENR |= (1<<0);					// Taktversorgung für Port A
-	RCC->AHB1ENR |= (1<<3);					// Taktversorgung für Port D
+	RCC->AHB1ENR |= (1<<0);				// Taktversorgung für Port A
+	RCC->AHB1ENR |= (1<<3);				// Taktversorgung für Port D
 
-	GPIOD->MODER |= (1<<26);				//Setze GPIO 13 auf Output
+	GPIOD->MODER |= (1<<26);			//Setze GPIO 13 auf Output
 
-	EXTI->IMR |= (1<<0);						// EXTI0 Maskierung aufheben
+	EXTI->IMR |= (1<<0);				// EXTI0 Maskierung aufheben
 
-	EXTI->RTSR |= (1<<0); 					// EXTI0 auf steigende Flanke setzen
+	EXTI->RTSR |= (1<<0); 				// EXTI0 auf steigende Flanke setzen
 	
 	
 	NVIC_EnableIRQ(EXTI0_IRQn);			//NVIC für EXTI0 einschalten
 	// Oder: NVIC->ISER[0] |= (1<<6);
 	
-	NVIC_SetPriority(EXTI0_IRQn,0);	// Priorität von EXTI 0 auf null setzen
+	NVIC_SetPriority(EXTI0_IRQn,0);		// Priorität von EXTI 0 auf null setzen
 	// Oder: NVIC->IP[4*6] |= (0<<0);
 	
 	//---MAIN LOOP---
