eb8d945df13b11af7127360dc44adccd24581a00
flo-sem <es22015@lehre.dhbw-stuttgart.de>
2023-11-13 11:04:51 +0100

    update 1

--- Aufgabe4/main.c
+++ Aufgabe4/main.c
@@ -1,31 +1,41 @@
 //---INCLUDES---
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
+
 //---HEADER---
 #include "main.h"
-// Projekt für Aufgabe 3
+// Projekt fÃ¼r Aufgabe 3
 // 		Aufgaben:
 //		- 	Effiziente FIFO Struktur implementieren
 //		-   Aufgabe 3 von Taster Event auf FIFO umbauen
 
+#define QUEUE_LENGTH = 5
+static element queue[5];
 
 void delay (int time) {
 	for (int i=0; i<10000*time; ++i) {}		
 }
 
-void EXTI0_IRQHandler() {
-	// Auch mölich: NVIC->ICER[0] |= (1<<6);
-	if(EXTI->PR & EXTI_PR_PR0) {		// EXTI_PR wird 1 wenn der Interrupt ausgelöst wird
+void push (uint32_t val){
+	for(uint32_t index = 0;index<5;++index) {
+		if(queue[index].intterrupt_ID == 0) {
+			queue[index].intterrupt_ID = val;
+		}
+	}
+}
 
-		EXTI->PR |= EXTI_PR_PR0;			// EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
+uint32_pull (
+void EXTI0_IRQHandler() {
+	// Auch mÃ¶lich: NVIC->ICER[0] |= (1<<6);
+	if(EXTI->PR & EXTI_PR_PR0) {		// EXTI_PR wird 1 wenn der Interrupt ausgelÃ¶st wird
+		EXTI->PR |= EXTI_PR_PR0;			// EXTI_PR wird zurÃ¼ckgesetzt, wenn eine 1 reingeschrieben wird
 	}
 }
 
 
-
 int main() {
-	RCC->AHB1ENR |= (1<<0);					// Taktversorgung für Port A
-	RCC->AHB1ENR |= (1<<3);					// Taktversorgung für Port D
+	RCC->AHB1ENR |= (1<<0);					// Taktversorgung fÃ¼r Port A
+	RCC->AHB1ENR |= (1<<3);					// Taktversorgung fÃ¼r Port D
 
 	GPIOD->MODER |= (1<<26);				//Setze GPIO 13 auf Output
 
@@ -34,15 +44,18 @@ int main() {
 	EXTI->RTSR |= (1<<0); 					// EXTI0 auf steigende Flanke setzen
 	
 	
-	NVIC_EnableIRQ(EXTI0_IRQn);			//NVIC für EXTI0 einschalten
+	NVIC_EnableIRQ(EXTI0_IRQn);			//NVIC fÃ¼r EXTI0 einschalten
 	// Oder: NVIC->ISER[0] |= (1<<6);
 	
-	NVIC_SetPriority(EXTI0_IRQn,0);	// Priorität von EXTI 0 auf null setzen
-	// Oder: NVIC->IP[4*6] |= (0<<0);
+	NVIC_SetPriority(EXTI0_IRQn,0);	// PrioritÃ¤t von EXTI 0 auf null setzen
+	// Oder: NVIC->IP[4*6] |= (0<<0);	
 	
+	
+	void pop () {
+	}
 	//---MAIN LOOP---
 	while (1) {
-		NVIC_EnableIRQ(EXTI0_IRQn);		//NVIC für EXTI0 einschalten
+		NVIC_EnableIRQ(EXTI0_IRQn);		//NVIC fÃ¼r EXTI0 einschalten
 		// Oder: NVIC->ISER[0] |= (1<<6);
 		delay(5);
 		//Logik zum toggeln der LED
--- Aufgabe4/main.h
+++ Aufgabe4/main.h
@@ -5,3 +5,8 @@ typedef struct node{
 	uint32_t data;		//Daten an der Stelle der Liste
 	struct node *next;	//Rekursive definition des Structs
 } node_t;
+
+typedef struct element {
+	uint32_t intterrupt_ID;
+	uint32_t next_index;
+} element;
