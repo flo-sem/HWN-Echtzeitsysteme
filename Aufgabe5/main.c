d15b9d406ee6e5fb588fde2b41e13e8c7b8756e6
flo-sem <florian.semen@karcher.com>
2024-03-11 13:53:01 +0100

    Solved Task 5

--- Aufgabe5/main.c
+++ Aufgabe5/main.c
@@ -9,59 +9,59 @@
 //		-  TIM4 nach vorgaben konfigurieren
 
 volatile static int state = 0;	//0=main , 1=interrupt
-static uint32_t running_main = 0;
-static uint32_t running_interrupt = 0;
 
-void delay (int time) {
-	for (int i=0; i<10000*time; ++i) {}		
+void TIM4_Configuration() {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; // Enable Timer 4 clock
+	TIM4->CR1 |= TIM_CR1_CEN; // Aktiviere Timer 4
+
+    TIM4->PSC = 16000 - 1 ; // Prescaler auf 1ms basis setzen
+    TIM4->ARR = 1000 - 1; // Mit Auto-Reload Register 500ms perioden festlegen
+
 }
 
 void EXTI0_IRQHandler() {
-	running_main = 0;
-	running_interrupt = 1;
-	//NVIC_DisableIRQ(EXTI0_IRQn);		// NVIC für EXTI0 ausschalten
-	// Auch mölich: NVIC->ICER[0] |= (1<<6);
-	delay(25);
-	if(EXTI->PR & EXTI_PR_PR0) {		// EXTI_PR wird 1 wenn der Interrupt ausgelöst wird
-		++state;
-		state=state%2;
-		EXTI->PR |= EXTI_PR_PR0;			// EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
-	}
+    if (EXTI->PR & EXTI_PR_PR0) { // EXTI_PR wird 1 wenn der Interrupt ausgelöst wird
+        ++state;
+        state = state % 2;
+        EXTI->PR |= EXTI_PR_PR0;  // EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
+    }
 }
 
 
 
 int main() {
+	TIM4_Configuration();
+
 	RCC->AHB1ENR |= (1<<0);					// Taktversorgung für Port A
 	RCC->AHB1ENR |= (1<<3);					// Taktversorgung für Port D
 
 	GPIOD->MODER |= (1<<26);				//Setze GPIO 13 auf Output
 
-	EXTI->IMR |= (1<<0);						// EXTI0 Maskierung aufheben
+	EXTI->IMR |= (1<<0);					// EXTI0 Maskierung aufheben
 
 	EXTI->RTSR |= (1<<0); 					// EXTI0 auf steigende Flanke setzen
 	
 	
 	NVIC_EnableIRQ(EXTI0_IRQn);			//NVIC für EXTI0 einschalten
 	// Oder: NVIC->ISER[0] |= (1<<6);
-	
+	int blink = 0;
 	NVIC_SetPriority(EXTI0_IRQn,0);	// Priorität von EXTI 0 auf null setzen
 	// Oder: NVIC->IP[4*6] |= (0<<0);
-	
 	//---MAIN LOOP---
 	while (1) {
-		running_main = 1;
-		running_interrupt = 0;
-		NVIC_EnableIRQ(EXTI0_IRQn);		//NVIC für EXTI0 einschalten
-		// Oder: NVIC->ISER[0] |= (1<<6);
-		delay(5);
-		//Logik zum toggeln der LED
-		if(state == 0) {
-			GPIOD->BSRR |= (1<<13);
-		}
-		else {
+		if(state == 0 ){
+			if(TIM4->SR & TIM_SR_UIF){
+				if(blink == 0){
+					blink = 1;
+					GPIOD->BSRR |= (1<<29);
+				}else{
+					blink = 0;
+					GPIOD->BSRR |= (1<<13);
+				}
+				TIM4->SR &= ~(TIM_SR_UIF); // Clear UIF
+			} 
+		} else {
 			GPIOD->BSRR |= (1<<29);
 		}
 	}
-
 }
