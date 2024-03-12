b57f2ac3b3415965124353c6e68577a67c849c36
robinpfister <119607482+robinpfister@users.noreply.github.com>
2024-03-12 16:24:51 +0100

    implement functions

--- Aufgabe11/drivers/drv_Timer.c
+++ Aufgabe11/drivers/drv_Timer.c
@@ -1,1 +1,36 @@
 #include "drv_Timer.h"
+#include <stddef.h>
+
+extern uint32_t pwm_value;
+
+void drv_timer4_init(void)
+{
+	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; // Enable Timer 4 clock
+    TIM4->CR1 |= TIM_CR1_CEN;           // Enable Timer 4
+
+    TIM4->PSC = 1600 - 1;  // Set prescaler to 1600 (divide clock by 1600) -> clock 10kHz
+    TIM4->ARR = 100 - 1;   // Set auto-reload register to 100
+	
+	// Set PWM mode 1 (OC1M bits 110)
+	TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; 
+    TIM4->CCMR1 &= ~TIM_CCMR1_OC1M_0;
+
+    TIM4->CCER |= TIM_CCER_CC1E; // Enable Capture/Compare Channel 1
+
+    TIM4->DIER &= ~TIM_DIER_UIE; // Disable update interrupt (Update-Interrupt deaktivieren) ????
+}
+
+void drv_timer4_CB_init(void(*callback)(void))
+{
+	if(!(callback == NULL))
+	{
+	callback();
+	}
+}
+
+void drv_timer4_PWM(uint32_t uiPWM_Start, uint32_t uiPWM_Value)
+{
+	TIM4->CCR1 = uiPWM_Start;
+	pwm_value = uiPWM_Value;
+} 
+
--- Aufgabe11/drivers/drv_Timer.h
+++ Aufgabe11/drivers/drv_Timer.h
@@ -1,7 +1,18 @@
 /* Include System Libaries */
 #include "stm32f4xx.h"
 
+
+
 /*Externe (globale) Funktionen zur Meldung eines periodischen Taktsignals und der Augabe eines PWM-Signals: */
+/**
+ * @brief initialize timer4.
+ * @return void.
+ */
 void drv_timer4_init(void);
-void drv_timer4_CB_init(void(*)(void));
+
+/**
+ * @brief initialize timer 4.
+ * @return void.
+ */
+void drv_timer4_CB_init(void(*callback)(void));
 void drv_timer4_PWM(uint32_t uiPWM_Start, uint32_t uiPWM_Value);
