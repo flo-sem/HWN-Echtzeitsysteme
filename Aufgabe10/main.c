9a4ffc522ef721318b1fc82c2465172e64c4a4cb
flo-sem <florian.semen@karcher.com>
2024-03-12 14:11:35 +0100

    Taks 10 Working

--- Aufgabe10/main.c
+++ Aufgabe10/main.c
@@ -2,101 +2,77 @@
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
 
-// Projekt für Aufgabe 6
-// 		Aufgaben:
-//		-  TIM4 soll jetzt einen Interrupt auslösen
-//		-  Interrup in main bearbeiten
+// Projekt für Aufgabe 10
+// Aufgaben: PWM-Steuerung der LED 4 mit dem User-Button
 
-volatile static int state = 0;	
-//event
-volatile static int e_timer = 0;
-
-//trace variablen
-volatile static int t_buttonInterrupt = 0;
-volatile static int t_timerInterrupt = 0;
-
-
-void delay (int time) {
-	for (int i=0; i<10000*time; ++i) {}		
-}
-
+volatile static uint16_t pwmValue = 40;
 
 void TIM4_Configuration() {
     RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; // Enable Timer 4 clock
-	TIM4->CR1 |= TIM_CR1_CEN; // Aktiviere Timer 4
+    TIM4->CR1 |= TIM_CR1_CEN;           // Aktiviere Timer 4
 
-    TIM4->PSC = 16000 - 1 ; // Prescaler auf 1ms basis setzen
-    TIM4->ARR = 1000 - 1; // Mit Auto-Reload Register 500ms perioden festlegen
+    TIM4->PSC = 1600 - 1;  // Prescaler auf 1ms Basis setzen
+    TIM4->ARR = 100 - 1;   // Auto-Reload Register auf 100 setzen (1ms)
 
-	TIM4->DIER |= TIM_DIER_UIE; // Zur verwendung des Interrupts
+    // Configure Capture/Compare Einheit 1 im PWM-Modus
+    TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // PWM mode 1
+    TIM4->CCMR1 &= ~TIM_CCMR1_OC1M_0; // Clear OC1M bits
+
+    TIM4->CCR1 = pwmValue; // Setze den PWM-Wert
+
+    TIM4->CCER |= TIM_CCER_CC1E; // Enable Capture/Compare Channel 1
+
+	TIM4->DIER &= ~TIM_DIER_UIE; // Disable update interrupt
 }
 
 void EXTI0_IRQHandler() {
-	t_timerInterrupt = 0;
-	t_buttonInterrupt = 1;
     if (EXTI->PR & EXTI_PR_PR0) { // EXTI_PR wird 1 wenn der Interrupt ausgelöst wird
-        ++state;
-        state = state % 2;
         EXTI->PR |= EXTI_PR_PR0;  // EXTI_PR wird zurückgesetzt, wenn eine 1 reingeschrieben wird
+
+        // Tastendruck = +10% PWM mit Überlauf von 100% auf 0%
+        pwmValue += 10;
+        if (pwmValue > 100) {
+            pwmValue = 0;
+        }
+        TIM4->CCR1 = pwmValue; // Aktualisiere den PWM-Wert
     }
-	delay(10);
-	t_timerInterrupt = 0;
-	t_buttonInterrupt = 0;
 }
-
-void TIM4_IRQHandler() {
-	t_timerInterrupt = 1;
-	t_buttonInterrupt = 0;
-
-    if (TIM4->SR & TIM_SR_UIF) {		
-		e_timer = 1;
-        TIM4->SR &= ~TIM_SR_UIF; // Clear UIF
-    }
-	delay(10);
-	t_timerInterrupt = 0;
-	t_buttonInterrupt = 0;
-}
-
 
 
 int main() {
-	t_timerInterrupt = 0;
-	t_buttonInterrupt = 0;
-	TIM4_Configuration();
 
 	RCC->AHB1ENR |= (1<<0);					// Taktversorgung für Port A
 	RCC->AHB1ENR |= (1<<3);					// Taktversorgung für Port D
 
-	GPIOD->MODER |= (1<<26);				//Setze GPIO 13 auf Output
+	//GPIOD->MODER |= (1<<26);				//Setze GPIO 13 auf Output
 
 	EXTI->IMR |= (1<<0);					// EXTI0 Maskierung aufheben
 
 	EXTI->RTSR |= (1<<0); 					// EXTI0 auf steigende Flanke setzen
 	
+	// Konfiguriere den GPIO-Pin (PD12) für alternative Funktion (AF2)
+	GPIOD->AFR[1] |= (2 << 16);  // Alternative Funktion AF2 für PD12 (TIM4_CH1)
+
+	// Konfiguriere PD12 (LED 4) für den alternativen Push-Pull-Modus
+	GPIOD->MODER |= (2 << (2 * 12));  // Alternative Funktion (AF2) für PD12
+	GPIOD->OTYPER &= ~(1 << 12);      // Push-Pull-Modus
+	GPIOD->OSPEEDR |= (3 << (2 * 12)); // High-Speed
+	
+	// Aktiviere den Kanal 1 der Capture/Compare-Einheit
+	TIM4->CCER |= TIM_CCER_CC1E;
+	
+	// Verknüpfe den GPIO-Pin (PD12) mit der Capture/Compare-Einheit 1 (TIM4_CH1)
+	TIM4->CCMR1 |= TIM_CCMR1_CC1S_0;  // CC1S = 01: CC1 Channel is configured as output
+
+	
 	/* NVIC SETUP */
 	NVIC_EnableIRQ(EXTI0_IRQn);			//NVIC für EXTI0 einschalten
 	NVIC_SetPriority(EXTI0_IRQn,1);	// Priorität von EXTI 0 auf null setzen
 	
-	NVIC_EnableIRQ(TIM4_IRQn); // NVIC für TIM4 einschalten
-    NVIC_SetPriority(TIM4_IRQn, 0); // Priorität von TIM4 auf eins setzen (geringer als EXTI0)
-	
-	int blink = 0;
+	TIM4_Configuration(); // Konfiguriere Timer 4 für PWM
 
 	//---MAIN LOOP---
 	while (1) {
-		if(state == 0 ){
-			if(e_timer){
-				if(blink == 0){
-					blink = 1;
-					GPIOD->BSRR |= (1<<29);
-				}else{
-					blink = 0;
-					GPIOD->BSRR |= (1<<13);
-				}
-				e_timer = 0;
-			} 
-		} else {
-			GPIOD->BSRR |= (1<<29);
-		}
+
 	}
 }
