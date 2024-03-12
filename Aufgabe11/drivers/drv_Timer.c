7645be1c36561d60696f67ce217db50b39216ff0
robinpfister <119607482+robinpfister@users.noreply.github.com>
2024-03-12 16:47:04 +0100

    code cleanup

--- Aufgabe11/drivers/drv_Timer.c
+++ Aufgabe11/drivers/drv_Timer.c
@@ -1,7 +1,7 @@
 #include "drv_Timer.h"
 #include <stddef.h>
 
-extern uint32_t pwm_value;
+extern uint16_t pwmValue;
 
 void drv_timer4_init(void)
 {
@@ -11,12 +11,10 @@ void drv_timer4_init(void)
     TIM4->PSC = 1600 - 1;  // Set prescaler to 1600 (divide clock by 1600) -> clock 10kHz
     TIM4->ARR = 100 - 1;   // Set auto-reload register to 100
 	
-	// Set PWM mode 1 (OC1M bits 110)
-	TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; 
-    TIM4->CCMR1 &= ~TIM_CCMR1_OC1M_0;
+	TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 & ~TIM_CCMR1_OC1M_0; // Set PWM mode 1 (OC1M bits 110)
+    //TIM4->CCMR1 &= ~TIM_CCMR1_OC1M_0;
 
     TIM4->CCER |= TIM_CCER_CC1E; // Enable Capture/Compare Channel 1
-
     TIM4->DIER &= ~TIM_DIER_UIE; // Disable update interrupt (Update-Interrupt deaktivieren) ????
 }
 
@@ -31,6 +29,6 @@ void drv_timer4_CB_init(void(*callback)(void))
 void drv_timer4_PWM(uint32_t uiPWM_Start, uint32_t uiPWM_Value)
 {
 	TIM4->CCR1 = uiPWM_Start;
-	pwm_value = uiPWM_Value;
+	pwmValue = uiPWM_Value;
 } 
 
