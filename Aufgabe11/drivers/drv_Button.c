07d0a72eb688e650470f37c666c97c1f986c7ee6
flo-sem <florian.semen@karcher.com>
2024-03-12 15:54:05 +0100

    finished drv_LED and drv_Button

--- Aufgabe11/drivers/drv_Button.c
+++ Aufgabe11/drivers/drv_Button.c
@@ -1,1 +1,20 @@
 #include "drv_Button.h"
+#include "stm32f4xx.h"
+
+void drv_button_init(void) {
+	RCC->AHB1ENR |= (1<<0);			// Taktversorgung für Port A
+	EXTI->IMR |= (1<<0);			// EXTI0 Maskierung aufheben
+	EXTI->RTSR |= (1<<0); 			// EXTI0 auf steigende Flanke setzen
+}
+
+void drv_button_CB_pressed_init(void (*callback)(void)){
+	if (callback != 0){             // Prüft auf einen validen Callback      
+        callback();
+    }	
+}
+
+void drv_button_CB_released_init(void (*callback)(void)) {
+	if (callback != 0) {            // Prüft auf einen validen Callback 
+        callback();
+    }
+}
