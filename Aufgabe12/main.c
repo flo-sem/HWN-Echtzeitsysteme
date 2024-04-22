f18a33b2a9b98da21e3c4ef755b1b5333ad17d32
flo-sem <florian.semen@karcher.com>
2024-04-22 11:41:13 +0200

    added state_machine.h

--- Aufgabe12/main.c
+++ Aufgabe12/main.c
@@ -1,27 +1,9 @@
-/* Include Drivers */
-#include "drivers/drv_Button.h"
-#include "drivers/drv_LED.h"
-#include "drivers/drv_Timer.h"
-
-uint32_t e_pwm_value = 0;  // global variable for current pwm value
-uint32_t e_pwm_increment = 0; // global variable for the pwm incrementation
-
-void IncreasePWM() {
-	// Button press = +10% PWM with overflow from 100% to 0% (Tastendruck = +10% PWM mit Überlauf von 100% auf 0%)
-    e_pwm_value += e_pwm_increment;
-    if (e_pwm_value > 100) {
-        e_pwm_value = 0;
-    }
-}
-
+/* Includes */
+#include "main.h"
+#include "state_machine.h"
 
 int main() {
-	drv_led_init();
-    drv_button_init();
-	drv_button_CB_pressed_init(IncreasePWM);
-	drv_timer4_init();
-	drv_timer4_PWM(100, 10);
-	
+
     while (1) {
         // Main program logic here (Hauptprogramm-Logik hier)
     }
